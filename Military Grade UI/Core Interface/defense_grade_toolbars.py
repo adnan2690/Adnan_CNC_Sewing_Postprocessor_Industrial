c8c850a76d3be1c90bfba47fe2c905fc9ecfa324
adnan2690 <56668818+adnan2690@users.noreply.github.com>
2025-06-10 18:34:38 +0300

    Update defense_grade_toolbars.py
    Signed-off-by: adnan2690 <56668818+adnan2690@users.noreply.github.com>

--- Military Grade UI/Core Interface/defense_grade_toolbars.py
+++ Military Grade UI/Core Interface/defense_grade_toolbars.py
@@ -1,15 +1,21 @@
-from PyQt5.QtWidgets import QToolBar, QAction
+from PyQt5.QtWidgets import QToolBar, QAction, QComboBox, QLabel
 from PyQt5.QtGui import QIcon
 
 class IndustrialToolbar(QToolBar):
-    def __init__(self, title):
-        super().__init__(title)
+    def __init__(self, title, parent=None):
+        super().__init__(title, parent)
         self.setMovable(False)
+        self.setIconSize(QSize(32, 32))
+        self._setup_style()
+    
+    def _setup_style(self):
+        """Apply defense-grade styling"""
         self.setStyleSheet("""
             QToolBar {
                 background-color: #34495e;
                 border: none;
                 padding: 5px;
+                spacing: 5px;
             }
             QToolButton {
                 background-color: #2c3e50;
@@ -21,10 +27,31 @@ class IndustrialToolbar(QToolBar):
             QToolButton:hover {
                 background-color: #3498db;
             }
+            QComboBox {
+                background-color: #2c3e50;
+                color: #ecf0f1;
+                border: 1px solid #3498db;
+                padding: 5px;
+                min-width: 120px;
+            }
         """)
     
     def add_tool(self, icon_path, tooltip, callback):
+        """Add industrial tool"""
         action = QAction(QIcon(icon_path), tooltip, self)
         action.triggered.connect(callback)
         self.addAction(action)
         return action
+    
+    def add_combo(self, label, items, callback):
+        """Add industrial combobox"""
+        self.addWidget(QLabel(label))
+        combo = QComboBox()
+        combo.addItems(items)
+        combo.currentIndexChanged.connect(callback)
+        self.addWidget(combo)
+        return combo
+    
+    def add_separator(self):
+        """Add industrial separator"""
+        self.addSeparator()
